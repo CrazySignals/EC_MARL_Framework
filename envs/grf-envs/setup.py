f651701fe5727510f83b2c4d29c4da20f9cd91fb
CrazySignals <1271366280@qq.com>
2025-04-01 11:40:28 +0800

    20250401

---
+++ envs/grf-envs/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='grf_envs',
+      packages=[
+   "grf_envs"
+   ],
+      version='0.0.1',
+      install_requires=['gym','numpy', 'gfootball']  
+)  
